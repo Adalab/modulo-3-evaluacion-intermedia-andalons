ebee8dd629dce98f0b02573a8ec3073bb9fed97a
andalons <andreacor001@gmail.com>
2022-04-26 09:04:17 +0200

    render initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-/* import logo from '../images/logo.png' */
 import '../styles/App.scss';
+import friendsData from '../data/friendsData.json';
+import { useState } from 'react';
 
 function App() {
+  //constantes de estad
+  const [initialData, setInitialData] = useState(friendsData);
+  const [newData, setNewData] = useState({
+    quote: '',
+    character: '',
+  });
+
+  //funciones manejadoras
+
+  //render
+  const renderHTML = () => {
+    return initialData.map((object, i) => (
+      <li key={i}>
+        <p>{object.quote}</p>
+        <p>{object.character}</p>
+      </li>
+    ));
+  };
+
   return (
     <div>
-      {/*   <img src={logo} className="App-logo" alt="logo"/> */}
-      <h1>Hola mundo!</h1>
+      <h1>Frases de Friends</h1>
+      <ul>{renderHTML()}</ul>
     </div>
   );
 }
