f545881db3f5089adb0fd14d761991dd834b26db
andalons <andreacor001@gmail.com>
2022-04-26 08:46:00 +0200

    add starter kit to project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+/* import logo from '../images/logo.png' */
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      {/*   <img src={logo} className="App-logo" alt="logo"/> */}
+      <h1>Hola mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
